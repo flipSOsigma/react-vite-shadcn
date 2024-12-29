e8ef03d4d94fc4fd4e48727f53775e75051597c7
alif mahendra <nacubes20@gmail.com>
2024-12-29 16:53:01 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex items-center justify-center'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <b>{count}</b>
+      <div className="card">
+        <Button variant={"outline"} onClick={() => setCount((count) => count + 1)}>Button</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        edited by flip so sigma
+      </p>
+    </>
+  )
+}
+
+export default App
